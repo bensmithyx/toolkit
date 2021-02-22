aacd66cd5533ec65338fb974f484e7559ff758ac
cyb3rchad <cyberchad@outlook.com>
2021-02-22 19:14:44 +0000

    adeed wordlists folder

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -49,7 +49,7 @@ def scanner(ip):
         print(f"PORT    SERVICE\n")
         file.write(f"PORT    SERVICE\n{30*'-'}")
         file.close()
-        for port in range(1,10000):
+        for port in range(65535):
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             result = sock.connect_ex((ip, port))
             if result == 0:
