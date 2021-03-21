24b0e42595409494c6fd247864b1b11f99408a31
cyb3rchad <cyb3rchad@tlookcom>
2021-03-21 15:01:26 +0000

    Added exit when choosing wordlist

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -264,9 +264,13 @@ def main(argv):
                         for index, list in enumerate(directory, 1):
                             if list[-4:] == ".txt":
                                 print(f"{index} - {list}")
-                        choice = getinput("wordlists", len(directory))-1
-                        wordlist = directory[choice]
-                        display(f"Set wordlist to {Colour.Red}{wordlist}{Colour.Reset}")
+                        print(f"{len(directory)+1} - Exit\n")
+                        choice = getinput("wordlists", len(directory)+1)-1
+                        if choice == len(directory):
+                            display("Exited")
+                        else:
+                            wordlist = directory[choice]
+                            display(f"Set wordlist to {Colour.Red}{wordlist}{Colour.Reset}")
                     elif choice == 2:
                         display("Exited")
                 elif option == 4:
